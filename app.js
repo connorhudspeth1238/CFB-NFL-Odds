53a7ffe330dcebd0fe345f86000c209120a1a8ee
connorhudspeth1238 <connorhudspeth1238@gmail.com>
2026-09-04 14:47:27 -0500

    Fix team names in conferenceTeams mapping

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ let currentCfbGroup = '81'; // Default to Top 25
 const conferenceTeams = {
   '8': [ // SEC
     'Alabama', 'Arkansas', 'Auburn', 'Florida', 'Georgia', 'Kentucky', 
-    'LSU', 'Mississippi St.', 'Missouri', 'Oklahoma', 'Ole Miss', 
+    'LSU', 'Mississippi State', 'Mississippi St.', 'Missouri', 'Oklahoma', 'Ole Miss', 
     'South Carolina', 'Tennessee', 'Texas', 'Texas A&M', 'Vanderbilt'
   ],
   '4': [ // Big Ten
-    'Illinois', 'Indiana', 'Iowa', 'Maryland', 'Michigan', 'Michigan St.', 
+    'Illinois', 'Indiana', 'Iowa', 'Maryland', 'Michigan', 'Michigan State', 'Michigan St.', 
     'Minnesota', 'Nebraska', 'Northwestern', 'Ohio State', 'Oregon', 
     'Penn State', 'Purdue', 'Rutgers', 'UCLA', 'USC', 'Washington', 'Wisconsin'
   ],
   '12': [ // Big 12
-    'Arizona', 'Arizona St.', 'Baylor', 'BYU', 'UCF', 'Cincinnati', 
-    'Colorado', 'Houston', 'Iowa State', 'Kansas', 'Kansas St.', 
-    'Oklahoma St.', 'TCU', 'Texas Tech', 'Utah', 'West Virginia'
+    'Arizona', 'Arizona State', 'Arizona St.', 'Baylor', 'BYU', 'UCF', 'Cincinnati', 
+    'Colorado', 'Houston', 'Iowa State', 'Iowa St.', 'Kansas', 'Kansas State', 'Kansas St.', 
+    'Oklahoma State', 'Oklahoma St.', 'TCU', 'Texas Tech', 'Utah', 'West Virginia'
   ],
   '1': [ // ACC
-    'Boston College', 'California', 'Clemson', 'Duke', 'Florida State', 
+    'Boston College', 'California', 'Clemson', 'Duke', 'Florida State', 'Florida St.', 
     'Georgia Tech', 'Louisville', 'Miami', 'North Carolina', 'NC State', 
     'Pittsburgh', 'SMU', 'Stanford', 'Syracuse', 'Virginia', 'Virginia Tech', 'Wake Forest'
   ],
@@ -28,17 +28,17 @@ const conferenceTeams = {
     'North Texas', 'Rice', 'South Florida', 'Temple', 'Tulane', 'Tulsa', 'UAB', 'UTSA'
   ],
   '17': [ // Mountain West
-    'Air Force', 'Boise State', 'Colorado State', 'Fresno State', 'Hawaii', 
-    'Nevada', 'New Mexico', 'San Diego State', 'San Jose State', 'UNLV', 'Utah State', 'Wyoming'
+    'Air Force', 'Boise State', 'Boise St.', 'Colorado State', 'Colorado St.', 'Fresno State', 'Fresno St.', 'Hawaii', 
+    'Nevada', 'New Mexico', 'San Diego State', 'San Diego St.', 'San Jose State', 'San Jose St.', 'UNLV', 'Utah State', 'Utah St.', 'Wyoming'
   ],
   '18': [ // Sun Belt
-    'Appalachian State', 'Arkansas State', 'Coastal Carolina', 'Georgia Southern', 
-    'Georgia State', 'James Madison', 'Louisiana', 'Louisiana Tech', 'Marshall', 
-    'Old Dominion', 'South Alabama', 'Southern Miss', 'Texas State', 'Troy', 'UL Monroe'
+    'Appalachian State', 'Appalachian St.', 'Arkansas State', 'Arkansas St.', 'Coastal Carolina', 'Georgia Southern', 
+    'Georgia State', 'Georgia St.', 'James Madison', 'Louisiana', 'Louisiana Tech', 'Marshall', 
+    'Old Dominion', 'South Alabama', 'Southern Miss', 'Southern Mississippi', 'Texas State', 'Texas St.', 'Troy', 'UL Monroe'
   ],
   '20': [ // MAC
-    'Akron', 'Ball State', 'Bowling Green', 'Buffalo', 'Central Michigan', 
-    'Eastern Michigan', 'Kent State', 'Miami (OH)', 'Northern Illinois', 
+    'Akron', 'Ball State', 'Ball St.', 'Bowling Green', 'Buffalo', 'Central Michigan', 
+    'Eastern Michigan', 'Kent State', 'Kent St.', 'Miami (OH)', 'Northern Illinois', 
     'Ohio', 'Toledo', 'Western Michigan'
   ]
 };
